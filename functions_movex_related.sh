4ae87560a59859ec02282464ed1011efde625319
Uwe Sommerlatt <Uwe.Sommerlatt@ottogroup.de>
2016-08-02 17:54:48 +0200

    Restructure movex related functions

--- functions_movex_related.sh
+++ functions_movex_related.sh
@@ -1,20 +1,59 @@
-readonly MOVEX_REPOS=$HOME/repos
+__basename_to_upper() {
+  echo "$(basename $1 | tr '[:lower:]' '[:upper:]')"
+}
 
-# Folder layout for MOVEX repositories:
-#
-# $MOVE_REPOS/
-# ├── t1
-# │   ├── common
-# │   ├── data
-# │   ├── olddata
-# │   └── transtor
-# └── t2
-#     ├── common
-#     ├── data
-#     ├── olddata
-#     └── transtor
-# ...
+__show_branch() {
+  printf "* %-10s: %s\n" "$(__basename_to_upper $1)" "$(git -C $1 rev-parse --abbrev-ref HEAD)"
+}
 
+__clean_repo() {
+  echo "* Cleaning $(__basename_to_upper $1)"
+  git -C $1 clean -f -d
+}
+
+__check_branches() {
+  for path in "$AMOS_NAT" "${AMOS_NAT}-ng" "$AMOS_COMMON"; do
+    [ -e "$path" ] && __show_branch "$path"
+  done
+}
+
+__clean_repos() {
+  for path in "$AMOS_NAT" "${AMOS_NAT}-ng" "$AMOS_COMMON"; do
+    [ -e "$path" ] && __clean_repo "$path"
+  done
+}
+
+__continue_make() {
+  read -p 'Continue with project make (y/n) ? ' choice
+  case "$choice" in
+    y|Y ) echo true;;
+    * ) echo false;;
+  esac
+}
+
+__project_instance() {
+  echo $(basename $(dirname "$AMOS_NAT"))
+}
+
+__set_dbuser_suffix() {
+  local dbuser_suffix="_$(__project_instance)"
+  sed -ri "s/(<.*$1>\s+:\s+)([\"\']?)(\w+)([\"\']?)/\1\2\U\3$dbuser_suffix\4/" $AMOS_NAT/config/$2
+}
+
+__reload_environment() {
+  echo '* Reload environment'
+  unset WORKSPACE
+  source "${AMOS_NAT}/ci_support/set_env.sh"
+}
+
+__make_project() {
+  local make_cmd='make install'
+  case "$AMOS_PROJ" in
+    transtor|russia ) make_cmd='ci_support/ci_make.sh';;
+  esac
+  cd $AMOS_NAT
+  time $make_cmd
+}
 
 dp_test_environment() {
   if [ -z $AMOS_NAT ]; then
@@ -31,62 +70,38 @@ create_branch() {
     ruby $AMOS_HOME/common/ci_support/create_branch.rb -j usommerl -p $JIRA_PASSWORD $@
 }
 
-_basename_to_upper() {
-  echo "$(basename $1 | tr '[:lower:]' '[:upper:]')"
-}
+readonly MOVEX_REPOS=$HOME/repos
 
-_show_branch() {
-  printf "* %-10s: %s\n" "$(_basename_to_upper $1)" "$(git -C $1 rev-parse --abbrev-ref HEAD)"
-}
+# Folder layout for MOVEX repositories:
+#
+# $MOVE_REPOS/
+# ├── t1
+# │   ├── common
+# │   ├── data
+# │   ├── olddata
+# │   └── transtor
+# ├── t2
+# │   ├── common
+# │   ├── data
+# │   ├── olddata
+# │   └── transtor
+# └── s1
+#     ├── common
+#     ├── data
+#     ├── olddata
+#     └── sample
+# ...
 
-_clean_repo() {
-  echo "* Cleaning $(_basename_to_upper $1)"
-  git -C $1 clean -f -d
-}
-
-_check_branches() {
-  for path in "$AMOS_NAT" "${AMOS_NAT}-ng" "$AMOS_COMMON"; do
-    [ -e "$path" ] && _show_branch "$path"
+movex_aliases() {
+  local dir
+  for dir in $MOVEX_REPOS/*/; do
+    local no=$(basename $dir | sed -r 's/\w+([0-9]+)/\1/')
+    local inst=$(find $dir -maxdepth 1 -type d -iname transtor -o -iname sample -o -iname russia | sed -r 's/.*\/(\w+)$/\1/')
+    [[ -z "$no" || -z "$inst" ]] && continue
+    alias ${inst}${no}="unset WORKSPACE; source ${dir}${inst}/ci_support/set_env.sh; cd ${dir}${inst}"
   done
 }
-
-_clean_repos() {
-  for path in "$AMOS_NAT" "${AMOS_NAT}-ng" "$AMOS_COMMON"; do
-    [ -e "$path" ] && _clean_repo "$path"
-  done
-}
-
-_continue_make() {
-  read -p 'Continue with project make (y/n) ? ' choice
-  case "$choice" in
-    y|Y ) echo true;;
-    * ) echo false;;
-  esac
-}
-
-_project_instance() {
-  echo $(basename $(dirname "$AMOS_NAT"))
-}
-
-_set_dbuser_suffix() {
-  local dbuser_suffix="_$(_project_instance)"
-  sed -ri "s/(<.*$1>\s+:\s+)([\"\']?)(\w+)([\"\']?)/\1\2\U\3$dbuser_suffix\4/" $AMOS_NAT/config/$2
-}
-
-_reload_environment() {
-  echo '* Reload environment'
-  unset WORKSPACE
-  source "${AMOS_NAT}/ci_support/set_env.sh"
-}
-
-_make_project() {
-  local make_cmd='make install'
-  case "$AMOS_PROJ" in
-    transtor|russia ) make_cmd='ci_support/ci_make.sh';;
-  esac
-  cd $AMOS_NAT
-  time $make_cmd
-}
+movex_aliases
 
 generate_configs() {
   echo '* Generate configs'
@@ -97,8 +112,8 @@ generate_configs() {
     russia ) vm_properties='env_amosVirtualBox.yml';;
   esac
   cd $AMOS_NAT/config
-  _set_dbuser_suffix 'DBUSER' $vm_properties
-  _set_dbuser_suffix 'DBUSER_CM' $default_properties
+  __set_dbuser_suffix 'DBUSER' $vm_properties
+  __set_dbuser_suffix 'DBUSER_CM' $default_properties
   ruby generate_configs.rb -p "$default_properties,$vm_properties"
   git checkout $default_properties $vm_properties
   cd $AMOS_NAT
@@ -109,26 +124,14 @@ movex_make() {
     echo 'Project environment not set'
     return
   fi
-  local logfile="$MOVEX_REPOS/$(date --iso-8601=seconds)_$(_project_instance)_make.log"
-  ( _check_branches
-    if [ $(_continue_make) == false ]; then
+  local logfile="$MOVEX_REPOS/$(date --iso-8601=seconds)_$(__project_instance)_make.log"
+  ( __check_branches
+    if [ $(__continue_make) == false ]; then
       rm -f $logfile
       return
     fi
-    _clean_repos
+    __clean_repos
     generate_configs
-    _reload_environment
-    _make_project ) 2>&1 | tee $logfile
+    __reload_environment
+    __make_project ) 2>&1 | tee $logfile
 }
-
-_create_aliases() {
-  local dir
-  for dir in $MOVEX_REPOS/*/; do
-    local no=$(basename $dir | sed -r 's/\w+([0-9]+)/\1/')
-    local inst=$(find $dir -maxdepth 1 -type d -iname transtor -o -iname sample -o -iname russia | sed -r 's/.*\/(\w+)$/\1/')
-    [[ -z "$no" || -z "$inst" ]] && continue
-    alias ${inst}${no}="unset WORKSPACE; source ${dir}${inst}/ci_support/set_env.sh; cd ${dir}${inst}"
-  done
-}
-
-_create_aliases
