9c07739ee3701c1bfbb68337f178592eb03128cf
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2018-07-09 13:13:52 +0200

    Update docker and docker-compose aliases

--- aliases.sh
+++ aliases.sh
@@ -25,7 +25,8 @@ alias ll='ls -HA --group-directories-first'
 alias lt='ls -HAtr'
 
 # require root privileges
-alias docker='sudo docker'
+alias d='sudo docker'
+alias c='sudo docker-compose'
 alias vpnc='sudo vpnc'
 alias vpnc-disconnect='sudo vpnc-disconnect'
 
