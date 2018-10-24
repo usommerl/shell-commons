98706f5e59a49fc537eea9a4621205b0827a84e2
Uwe Sommerlatt <uwe.sommerlatt@exelonix.com>
2018-10-24 09:17:33 +0200

    Remove optional function keyword
    "The first form of the syntax is generally preferred
    because it's compatible with Bourne/Korn/POSIX scripts
    and so more portable."
    
    See: https://stackoverflow.com/a/7917086/1827123

--- functions.sh
+++ functions.sh
@@ -1,31 +1,31 @@
-function _listFilterBase() {
+_listFilterBase() {
     ls -lhHF $2 --color=always --group-directories-first \
                 --time-style=long-iso $1 | egrep -v '^total'
 }
 
-function _onlyHiddenFilter() {
+_onlyHiddenFilter() {
     egrep '.+[[:space:]]([[:cntrl:]]\[([[:digit:]]{2};)*[[:digit:]]{2}m)?\..+'
 }
 
-function _directoryFilter() { egrep $1 --color=never '^d'; }
+_directoryFilter() { egrep $1 --color=never '^d'; }
 
-function ldir() { _listFilterBase $1 | _directoryFilter; }
+ldir() { _listFilterBase $1 | _directoryFilter; }
 
-function lldir() { _listFilterBase $1 '-A' | _directoryFilter; }
+lldir() { _listFilterBase $1 '-A' | _directoryFilter; }
 
-function lhdir { lldir $1 | _onlyHiddenFilter; }
+lhdir() { lldir $1 | _onlyHiddenFilter; }
 
-function lf() { _listFilterBase $1 | _directoryFilter '-v'; }
+lf() { _listFilterBase $1 | _directoryFilter '-v'; }
 
-function llf() { _listFilterBase $1 '-A' | _directoryFilter '-v'; }
+llf() { _listFilterBase $1 '-A' | _directoryFilter '-v'; }
 
-function lhf() { llf $1 | _onlyHiddenFilter; }
+lhf() { llf $1 | _onlyHiddenFilter; }
 
-function lh() { _listFilterBase $1 '-A' | _onlyHiddenFilter; }
+lh() { _listFilterBase $1 '-A' | _onlyHiddenFilter; }
 
-function lz() { du -xahL -d 1 $1 2>/dev/null | sort -h; }
+lz() { du -xahL -d 1 $1 2>/dev/null | sort -h; }
 
-function gitignoreTemplate() {
+gitignoreTemplate() {
   if [ $# -gt 0 ]; then
       local baseURL='https://raw.githubusercontent.com/github/gitignore/master/'
       curl -fs ${baseURL}${1}.gitignore >> .gitignore
