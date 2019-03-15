53afd8cb6abe9efa74f8b2bcb90c0627cc5a9dbb
Uwe Sommerlatt <uwe.sommerlatt@exelonix.com>
2019-03-15 14:58:53 +0100

    Move host specific variables to a separate file

--- variables.sh
+++ variables.sh
@@ -1,21 +1,14 @@
-export JAVA_HOME=/usr/lib/jvm/default
-export EDITOR=nvim
 export MANPAGER='nvim -c "set ft=man" -'
+export EDITOR=nvim
 export LESS=-iR
-export DART_SDK=/opt/dart/dart-sdk/
-export GEM_HOME=$HOME/.gem/ruby/2.2.0
-export RUBY_GEM_EXECUTABLES=$HOME/.gem/ruby/2.2.0/bin
 export TEXMFHOME=$HOME/.texmf
 export MPD_HOST=/$HOME/.mpd/socket
-export TERM=rxvt-unicode-256color
-export GOPATH=$HOME/playground/go_workspace
+export TERM=rxvt-256color
 export PATH="${PATH}:$HOME/.local/bin"
 export PATH="${PATH}:$HOME/.cargo/bin"
-export PATH="${PATH}:$HOME/.yarn-global/bin"
-export PATH="${PATH}:$GOPATH/bin"
-export PATH="${PATH}:$DART_SDK/bin/"
-export PATH="${PATH}:$RUBY_GEM_EXECUTABLES"
 
-unset GIT_EDITOR
 
-# vim: set filetype=sh:
+# Host specific overrides
+FILENAME="$(dirname $BASH_SOURCE)/$(echo $(basename $BASH_SOURCE) | sed "s/\.sh/_$(hostname).sh/g")"
+[ -f "$FILENAME" ] && source "$FILENAME"
+unset FILENAME
