ca1b031a15aad76ac8ae08e3d843fbd8a73e2ea5
Uwe Sommerlatt <uwe.sommerlatt@exelonix.com>
2019-05-28 09:57:17 +0200

    Use separate secret files for different environments

--- functions_nereus.sh
+++ functions_nereus.sh
@@ -1,9 +1,10 @@
 
 unlockSecrets() {
+  local environment="$1"
   command -v dotenv-vault >/dev/null 2>&1 || { echo >&2 "doten-vault binary not installed"; return 1; }
   local password
   echo -n 'password:'
   read -s password
-  eval "$(dotenv-vault -k $password decrypt ~/.secrets | sed 's/^/export /' | tr '\n' ';')"
+  eval "$(dotenv-vault -k $password decrypt ~/.secrets_$environment | sed 's/^/export /' | tr '\n' ';')"
   echo
 }
