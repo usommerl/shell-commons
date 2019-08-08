49559f9774787e03c7a82e3759a6adb20ac263fa
Uwe Sommerlatt <uwe.sommerlatt@exelonix.com>
2019-08-08 16:24:26 +0200

    Add function to check consumer ofsets

--- functions_nereus.sh
+++ functions_nereus.sh
@@ -8,3 +8,17 @@ unlockSecrets() {
   eval "$(dotenv-vault -k $password decrypt ~/.secrets_$environment | sed 's/^/export /' | tr '\n' ';')"
   echo
 }
+
+consumerOffsets() {
+  local host="$1"
+  local configPath="$HOME/issues/IB-442/$host/client.properties"
+  local groups=''
+
+  groups="$(kafka-consumer-groups.sh --bootstrap-server "$host:9093" --command-config "$configPath" --list | grep 'egress\|router'| sort)"
+
+  while read -r group; do
+    kafka-consumer-groups.sh --bootstrap-server "$host:9093" --command-config "$configPath" --describe --group "$group"
+  done <<< "$groups"
+}
+
+export -f consumerOffsets
