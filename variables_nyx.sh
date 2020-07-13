630237d46901860cf719731a4349e6240f7101bc
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2020-07-13 20:33:39 +0200

    Use GEM home v2.7.0

--- variables_nyx.sh
+++ variables_nyx.sh
@@ -1,7 +1,7 @@
 export JAVA_HOME=/usr/lib/jvm/default
 export DART_SDK=/opt/dart/dart-sdk/
-export GEM_HOME=$HOME/.gem/ruby/2.2.0
-export RUBY_GEM_EXECUTABLES=$HOME/.gem/ruby/2.2.0/bin
+export GEM_HOME=$HOME/.gem/ruby/2.7.0
+export RUBY_GEM_EXECUTABLES=$HOME/.gem/ruby/2.7.0/bin
 export GOPATH=$HOME/playground/go_workspace
 
 export PATH="${PATH}:$HOME/.yarn-global/bin"
