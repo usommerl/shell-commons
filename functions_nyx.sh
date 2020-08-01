f57eab0a315de3e261f6261765845c336fdf9bc9
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2020-08-01 10:39:19 +0200

    Remove function flac2mp3

--- functions_nyx.sh
+++ functions_nyx.sh
@@ -9,33 +9,6 @@ requires() {
   done
 }
 
-flac2mp3() {
-  requires ffmpeg
-  requires parallel
-
-  fileList=()
-  local arg
-  for arg in $@; do
-      if [[ -d "$arg" ]]; then
-          local OLDIFS=$IFS
-          IFS=$'\n'
-          files=( $(find "${arg%/}/" -maxdepth 1 -type f -iname "*.flac") )
-          fileList=( ${fileList[@]} ${files[@]} )
-          IFS=$OLDIFS
-          unset files
-      elif [[ -f "$arg"  &&  "${arg##*.}" == "flac" ]]; then
-          fileList+=( "$arg" )
-      fi
-  done
-  parallel --no-notice \
-      'mp3File={.}.mp3;\
-       if [ -f "$mp3File" ]; then echo -e "\e[1;33m[Skipped]\e[0m $mp3File"; fi;\
-       ffmpeg -loglevel quiet -nostdin \
-              -i {} -f mp3 -vn -ab 320000 "$mp3File";\
-       if [ "$?" -eq 0 ]; then echo -e "\e[1;97m[Created]\e[0m $mp3File"; fi;' ::: "${fileList[@]}"
-  unset fileList
-}
-
 bctl() {
   case $1 in
   off)
