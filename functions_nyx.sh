ee3f6263e0cd71fc7b70324d4a414486834cf6f0
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2020-09-13 19:19:29 +0200

    Use helper function for bluetoothctl connect

--- functions_nyx.sh
+++ functions_nyx.sh
@@ -1,22 +1,22 @@
+__bctl_connect() {
+    bluetoothctl -- power off
+    bluetoothctl -- power on
+    bluetoothctl -- connect "$1"
+}
+
 bctl() {
   case $1 in
   off)
     bluetoothctl -- power off
     ;;
   btc22)
-    bluetoothctl -- power off
-    bluetoothctl -- power on
-    bluetoothctl -- connect 70:B3:D5:94:A3:32
+    __bctl_connect 70:B3:D5:94:A3:32
     ;;
   ueboom2)
-    bluetoothctl -- power off
-    bluetoothctl -- power on
-    bluetoothctl -- connect 88:C6:26:C5:52:9B
+    __bctl_connect 88:C6:26:C5:52:9B
     ;;
   aiaiai)
-    bluetoothctl -- power off
-    bluetoothctl -- power on
-    bluetoothctl -- connect 00:08:E0:73:07:70
+    __bctl_connect 00:08:E0:73:07:70
     ;;
   *)
     echo "Unknown argument: $1"
